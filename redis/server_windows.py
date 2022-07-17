f2ad3d41481cb7c4d56fcc19b8aa9680ff19947c
João Fraga <joaofraga@sparkyway.com>
2022-07-17 18:43:22 +0000

    Changed server lib funcs to match the linux bson lib

--- redis/server_windows.py
+++ redis/server_windows.py
@@ -2,22 +2,21 @@ import websocket
 import pprint
 import bson
 
+
 UNIVERSALLIS_SOCKET = "wss://universalis.app/api/ws"
 
 
 def on_message(wsapp, message):
-    pprint.pprint(bson.decode(message))
+    pprint.pprint(bson.loads(message))
 
 
 def subscribe(wsapp):
     print("Sending subscribe event")
-    print("test", bson.encode({"event": "subscribe", "channel": "listings/add"}))
-    wsapp.send(bson.encode({"event": "subscribe", "channel": "listings/add"}))
+    wsapp.send(bson.dumps({"event": "subscribe", "channel": "listings/add"}))
     print("Sent subscribe event")
 
 
 
-bson.encode({"event": "subscribe", "channel": "listings/add"})
 wsapp = websocket.WebSocketApp(
     UNIVERSALLIS_SOCKET, on_open=subscribe, on_message=on_message)
 wsapp.run_forever()
