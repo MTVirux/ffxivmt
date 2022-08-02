9075b0c33c59409c9f663c4ead6308286ed6b947
MTVirux <supersilver-dark@hotmail.com>
2022-08-02 11:05:49 +0000

    Added custom REDIS model core class

---
+++ backend/application/config/redis.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/**
+ * 
+ *  REDIS CONNECTION CONFIGURATION
+ * 
+ */
+$config['redis_sentinels'] = array(
+    'ffmt_redis'=> array(
+                        'scheme'    =>  'tcp',
+                        'host'      =>  'ffmt_redis',
+                        'port'      =>  '6379',
+                        )
+);
+$config['redis_port'] = 6379;
+$config['redis_timeout'] = 0;
+
+
+/**
+ * 
+ *      DATABASE INDEXES
+ * 
+ */
+$config['redis_listings_db'] = 0;
+$config['redis_sales_db'] = 1;
+$config['redis_listings_clean_db'] = 2;
+$config['redis_sales_clean_db'] = 3;
+$config['recent_db'] = 4;
---
+++ backend/application/core/MY_Redis_Model.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH.'vendor/predis/predis/autoload.php';
+
+class MY_Redis_Model extends CI_Model{
+
+    protected $redis;
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->config('redis');
+        Predis\Autoloader::register();
+        $this->redis = new Predis\Client([
+            'scheme' => 'tcp',
+            'host'   => 'ffmt_redis',
+            'port'   => 6379,
+        ]);
+        if($this->redis->ping()){
+            return $this->redis;
+        }else{
+            return false;
+        }
+    }
+}
+
