2a9ae3386f5a754fe02270005bf4cb652add4b80
MTVirux <supersilver-dark@hotmail.com>
2022-08-02 11:09:02 +0000

    Added Redis API controller for testing

---
+++ backend/application/controllers/API/Redis.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'vendor/chriskacerguis/codeigniter-restserver/src/RestController.php';
+require APPPATH.'vendor/chriskacerguis/codeigniter-restserver/src/Format.php';
+use chriskacerguis\RestServer\RestController;
+
+class Redis extends RestController {
+
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Redis/Redis_listings_model', 'Listings');
+        //$this->load->library('redis');
+    }
+
+    public function keys_get($world = null, $item_id = null){
+        $req = $this->get();
+        
+        if(!empty($req['world'])){
+            $world = $req['world'];
+        }
+        
+        if(!empty($req['item_id'])){
+            $item_id = $req['item_id'];
+        }
+        
+        $this->Listings->get_keys($world, $item_id);
+
+        if($data['success'] == 'true'){
+            $this->response([
+                            'status' => 'true',
+                            'data' => json_encode($data['data'])
+                        ], 200);
+        }else{
+            $this->response([
+                            'status' => 'false',
+                            'message' => $data['message']
+                        ], 404);
+        }
+    }
+
+    public function recent_get($world_id = null, $limit = null){
+        $req = $this->get();
+        
+        if(!empty($req['limit'])){
+            $limit = $req['limit'];
+        }
+
+        if(!empty($req['world'])){
+            $world_id = $req['world'];
+        }
+        
+        $data = $this->Listings->recent_apiGet($world_id, $limit);
+
+        if($data['success'] == 'true'){
+            $this->response([
+                            'status' => 'true',
+                            'data' => json_encode($data['data'])
+                        ], 200);
+        }else{
+            $this->response([
+                            'status' => 'false',
+                            'message' => $data['message']
+                        ], 404);
+        }
+    }
+}
+
