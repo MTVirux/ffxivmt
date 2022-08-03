3af7161a129e52d92150ffa7f294ae51ecaa1522
MTVirux <supersilver-dark@hotmail.com>
2022-08-03 14:49:37 +0000

    Updated listings_remove to match the redis JSON schema

--- docker/redis/server/listings_remove.py
+++ docker/redis/server/listings_remove.py
@@ -2,18 +2,27 @@ from base64 import decode
 from posixpath import split
 import websocket
 import bson
-import redis
 import json
 import config
 import database
+import log
+
+
+def remove_from_recent(world_name, listing_id):
+    list_name = "recent_" + world_name
+    if(database.DB_LISTINGS.lrem(list_name, 1, listing_id) > 0):
+        log.err_write("Removed listing " + listing_id + " from " + list_name)
+        log.action("{"+str(config.REDIS_LISTINGS_DB)+"}{LREM} Removed listing " + listing_id + " from " + list_name)
+    list_name = "recent_listings"
+    if(database.DB_LISTINGS.lrem(list_name, 1, listing_id) > 0):
+        log.err_write("Removed listing " + listing_id + " from " + list_name)
+        log.action("{"+str(config.REDIS_LISTINGS_DB)+"}{LREM} Removed listing " + listing_id + " from " + list_name)
 
 def handle_remove_listings(hash, listing):
-
     #Commit to db (1 = SUCESS, 0 = FAIL)
     #hdel(hash, field)
-    
-    if(database.DB_LISTING.hdel(str(hash), str(listing['listingID'])) == 1 ):
-        print("{0}{HDEL}Removed listing " + listing['listingID'] + " from " + hash)
+    if(database.DB_LISTINGS.json().delete(hash, listing['listingID']) > 0):
+        log.action("{"+str(config.REDIS_LISTINGS_DB)+"}{JSON_DEL} Removed listing " + listing['listingID'] + " from " + hash)
     return
 
 
@@ -29,11 +38,12 @@ def on_message(ws_listing_remove, message):
 
     
     for listing in listings:
-        if(type(listing['listingID']) != type(" ")):
+        if(listing['listingID'] is None):
             continue
         if(listing['listingID'] in config.BANNED_LISTING_IDS):
             continue
         handle_remove_listings(hash, listing)
+        remove_from_recent(world_name, listing['listingID'])
 
 
 def subscribe(ws_listing_remove):
@@ -41,9 +51,9 @@ def subscribe(ws_listing_remove):
         for k in config.WORLDS_TO_USE[i]:
             sub_list_value = config.WORLDS_TO_USE[i][k]
             world_id = "{world=" + str(k) + "}"
-            print("listings/remove" + world_id)
+            log.action("listings/remove" + world_id)
             ws_listing_remove.send(bson.encode({"event": "subscribe", "channel": "listings/remove" + world_id}))
-            print("Sent subscribe event for listings/remove on world " + sub_list_value + "(" + str(k) + ")")
+            log.action("Sent subscribe event for listings/remove on world " + sub_list_value + "(" + str(k) + ")")
 
 
 
