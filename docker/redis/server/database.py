e320807384c6a1848f12e64e203c9ed8d688868a
MTVirux <supersilver-dark@hotmail.com>
2022-08-03 10:21:52 +0000

    Changed listings db alias to plural

--- docker/redis/server/database.py
+++ docker/redis/server/database.py
@@ -1,8 +1,8 @@
 import redis
 import config
 
-DB_LISTING = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_LISTINGS_DB)
-DB_LISTING.client_setname("LISTINGS_CLIENT")
+DB_LISTINGS = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_LISTINGS_DB)
+DB_LISTINGS.client_setname("LISTINGS_CLIENT")
 DB_SALES = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_SALES_DB)
 DB_SALES.client_setname("SALES_CLIENT")
 DB_LISTINGS_CLEAN = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_LISTINGS_CLEANING_DB)
