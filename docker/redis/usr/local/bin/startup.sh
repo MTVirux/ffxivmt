82943bdb327b04f1445ac8880a666de1dc10a51f
MTVirux <supersilver-dark@hotmail.com>
2022-08-16 14:32:37 +0000

    Updated the startup script to set redis vars

--- docker/redis/usr/local/bin/startup.sh
+++ docker/redis/usr/local/bin/startup.sh
@@ -1,3 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-/Python-3.10.5/python /server/server.py
+/custom_entrypoint.sh &
+
+redis-cli config set dir /server/persistent_data
+redis-cli config set dbfilename $(echo `date +"%Y%m%d%H%M%S"`).rdb
+
+# Start the first process
+/Python-3.10.5/python /server/server.py &
+  
+# Start the second process
+/usr/local/bin/redis_memory_cleaner.sh &
+  
+# Wait for any process to exit
+while true;do
+    sleep 60
+done
+  
+# Exit with status of process that exited first
+exit $?
