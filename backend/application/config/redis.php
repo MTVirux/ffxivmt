60c336140985d07392b949e6299923c385ba5c2e
MTVirux <supersilver-dark@hotmail.com>
2022-08-16 14:28:57 +0000

    Changed DB indexes

--- backend/application/config/redis.php
+++ backend/application/config/redis.php
@@ -23,8 +23,8 @@ $config['redis_timeout'] = 0;
  *      DATABASE INDEXES
  * 
  */
-$config['redis_listings_db'] = 0;
-$config['redis_sales_db'] = 1;
-$config['redis_listings_clean_db'] = 2;
-$config['redis_sales_clean_db'] = 3;
+$config['redis_sales_db'] = 0;
+$config['redis_listings_db'] = 1;
+$config['redis_sales_clean_db'] = 2;
+$config['redis_listings_clean_db'] = 3;
 $config['recent_db'] = 4;
