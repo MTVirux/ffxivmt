9a8a4b2234eff5f37043b3e624db351e86d1b77c
MTVirux <supersilver-dark@hotmail.com>
2022-08-17 13:25:01 +0000

    Removed cleaning DBs

--- docker/redis/server/database.py
+++ docker/redis/server/database.py
@@ -5,7 +5,3 @@ DB_LISTINGS = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=con
 DB_LISTINGS.client_setname("LISTINGS_CLIENT")
 DB_SALES = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_SALES_DB)
 DB_SALES.client_setname("SALES_CLIENT")
-DB_LISTINGS_CLEAN = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_LISTINGS_CLEANING_DB)
-DB_LISTINGS_CLEAN.client_setname("LISTINGS_CLEAN_CLIENT")
-DB_SALES_CLEAN = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_SALES_CLEANING_DB)
-DB_SALES_CLEAN.client_setname("SALES_CLEAN_CLIENT")
