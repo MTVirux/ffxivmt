dfd4f06725f8d9735a137d7a31885495413860d4
MTVirux <supersilver-dark@hotmail.com>
2022-08-17 13:25:20 +0000

    Added "Recent" DB worker

--- docker/redis/server/database.py
+++ docker/redis/server/database.py
@@ -5,3 +5,5 @@ DB_LISTINGS = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=con
 DB_LISTINGS.client_setname("LISTINGS_CLIENT")
 DB_SALES = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_SALES_DB)
 DB_SALES.client_setname("SALES_CLIENT")
+DB_RECENT = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_RECENT_DB)
+DB_RECENT.client_setname("RECENT_CLIENT")
