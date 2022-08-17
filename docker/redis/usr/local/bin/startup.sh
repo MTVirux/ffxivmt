a065affce1af65a6a62785dafc22fff54c559e7b
MTVirux <supersilver-dark@hotmail.com>
2022-08-17 11:45:23 +0000

    Added comments for redis config commands

--- docker/redis/usr/local/bin/startup.sh
+++ docker/redis/usr/local/bin/startup.sh
@@ -2,6 +2,9 @@
 
 /entrypoint.sh &
 
+mkdir -p ${REDIS_DATA_DIR}  # Create data directory if it doesn't exist
+redis-cli config set dir ${REDIS_DATA_DIR} # Set data directory
+redis-cli config set dbfilename $(echo `date +"%Y%m%d%H%M%S"`).rdb #Set the name of the dump file to current timestamp
 redis-cli config set save "" #Disable automatic saving
 redis-cli select 0
 redis-cli set sales ${REDIS_SALES_DB}
