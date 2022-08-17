842bf52d787394166940f5c1adb85732d2e4b6ce
MTVirux <supersilver-dark@hotmail.com>
2022-08-17 11:35:08 +0000

    Fixed cleaner script

--- docker/redis/usr/local/bin/redis_memory_cleaner.sh
+++ docker/redis/usr/local/bin/redis_memory_cleaner.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-threshold=100          # Minimum amount of memory left when you should start killing, in MB
+THRESHOLD=100          # Minimum amount of memory left when you should start killing, in MB
 while true; do
-    available=$(free -m | head -2 | tail -1 | awk '{print $4}')
-    date = $(echo `date +"%Y%m%d%H%M%S"`)
-    if [ "$threshold" -ge "$available" ]; then
-        redis-cli config set dbfilename $(echo `date +"%Y%m%d%H%M%S"`).rdb
+    AVALIABLE=$(free -m | head -2 | tail -1 | awk '{print $4}')
+    DATE=$(echo `date +"%Y%m%d%H%M%S"`)
+    if [ "$THRESHOLD" -ge "$AVALIABLE" ]; then
+        redis-cli config set dbfilename $DATE.rdb
         redis-cli save
         redis-cli flushall
-        mv /rdump/dump.rdb /server/presistent_dumps/$date.rdb
         echo "Server memory is low, cleaning up..."
     fi
     sleep 20
