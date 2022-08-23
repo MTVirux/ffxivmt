e85e2435e659fbfc2b8f7e10075bcd7004d887c7
MTVirux <supersilver-dark@hotmail.com>
2022-08-23 11:17:33 +0000

    Memory cleaner overhaul to save CPU

--- docker/redis/usr/local/bin/redis_memory_cleaner.sh
+++ docker/redis/usr/local/bin/redis_memory_cleaner.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-THRESHOLD=100          # Minimum amount of memory left when you should start killing, in MB
+THRESHOLD=50
+
+# Get the total memory size
+MEMORY_LIMIT_IN_BYTES=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+MEMORY_LIMIT_IN_MEGABYTES=$(($MEMORY_LIMIT_IN_BYTES / 1024 / 1024))
+
+if [ ${REDIS_MEMORY_CLEANER_TRIGGER_TYPE} == "percentage" ];then
+    THRESHOLD=${MEMORY_LIMIT_IN_MEGABYTES}/100*${REDIS_MEMORY_CLEANER_THRESHOLD_PERCENTAGE}
+else
+    THRESHOLD=${REDIS_MEMORY_CLEANER_THRESHOLD_SIZE}
+fi
+
+
 rm -rf /server/logs/redis_memory_cleaner.log
 echo "---Memory cleaner started---" >> /server/logs/redis_memory_cleaner.log
 while true; do
-    MEMORY_LIMIT_IN_BYTES=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
-    MEMORY_LIMIT_IN_MEGABYTES=$(($MEMORY_LIMIT_IN_BYTES / 1024 / 1024))
+
+    # Get the used memory size
     MEMORY_USAGE_IN_BYTES=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes)
     MEMORY_USAGE_IN_MEGABYTES=$(($MEMORY_USAGE_IN_BYTES / 1024 / 1024))
+
+    # Get the percentage of memory used
     MEMORY_LEFT_IN_MEGABYTES=$(($MEMORY_LIMIT_IN_MEGABYTES - $MEMORY_USAGE_IN_MEGABYTES))
     MEMORY_LEFT_TO_HIT_THRESHOLD=$(($MEMORY_LEFT_IN_MEGABYTES - $THRESHOLD))
-    DATE=$(echo `date +"%Y%m%d%H%M%S"`)
+
     if [ "$THRESHOLD" -ge "$MEMORY_LEFT_IN_MEGABYTES" ]; then
+        DATE=$(echo `date +"%Y%m%d%H%M%S"`)
         redis-cli config set dbfilename $DATE.rdb
         redis-cli save
         redis-cli flushall
@@ -19,7 +34,8 @@ while true; do
         redis-cli set recent ${REDIS_RECENT_DB}
         MESSAGE="["+$date"]"+" Server memory is low, cleaning up..."
         echo $MESSAGE >> /server/logs/redis_memory_cleaner.log
+        echo $MESSAGE > /server/logs/status/redis_memory_cleaner.status
     fi
     echo "[" $(date) "]" "Current memory usage is: $MEMORY_USAGE_IN_MEGABYTES MB/ $MEMORY_LIMIT_IN_MEGABYTES MB ( $MEMORY_LEFT_TO_HIT_THRESHOLD MB left to hit threshold of $THRESHOLD MB)" >> /server/logs/redis_memory_cleaner.log
-    sleep 0.1
+    sleep ${REDIS_MEMORY_CLEANER_INTERVAL}
 done
