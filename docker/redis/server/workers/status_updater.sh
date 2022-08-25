0156f2d8a447f98a884cdc6022d1f86b3260e2d2
MTVirux <supersilver-dark@hotmail.com>
2022-08-25 10:56:09 +0000

    Updated status tracker script

--- docker/redis/server/workers/status_updater.sh
+++ docker/redis/server/workers/status_updater.sh
@@ -1,32 +1,132 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Must specify number of lines to show"
-  exit 1
+    NUMBER_OF_ACTION_LINE=1
+else
+    NUMBER_OF_ACTION_LINES=$1
 fi
 
-NUMBER_OF_LINES=$1
-LAST_LISTINGS_ADD_FILE=$(ls /server/logs/action -1|grep listings_add|tail -n 1)
-LAST_LISTINGS_REMOVE_FILE=$(ls /server/logs/action -1|grep listings_remove|tail -n 1)
-LAST_SALES_ADD_FILE=$(ls /server/logs/action -1|grep sales_add|tail -n 1)
+if [ -z "$2" ]; then
+    NUMBER_OF_DEBUG_LINES=1
+else
+    NUMBER_OF_DEBUG_LINES=$2
+fi
 
-mkdir -p /server/logs/status
+if [ -z "$3" ]; then
+    NUMBER_OF_ERROR_LINES=1
+else
+    NUMBER_OF_ERROR_LINES=$3
+fi
+
+if [ -z "$4" ]; then
+    NUMBER_OF_OTHER_LINES=1
+else
+    NUMBER_OF_OTHER_LINES=$4
+fi
+
+
+SALES_ADD_LOG_FILE=/server/logs/action/$(ls /server/logs/action -1|grep sales_add|tail -n 1)
+SALES_ADD_ERROR_LOG_FILE=/server/logs/error/$(ls /server/logs/error -1|grep sales_add|tail -n 1)
+SERVER_DEBUG_LOG_FILE=/server/logs/$(ls /server/logs -1|grep debug|tail -n 1)
+MEMORY_CLEANER_LOG_FILE=/server/logs/$(ls /server/logs -1|grep redis_memory_cleaner|tail -n 1)
+
+touch ${REDIS_SERVER_LOGS_DIR}/status/action.status
+touch ${REDIS_SERVER_LOGS_DIR}/status/error.status
+touch ${REDIS_SERVER_LOGS_DIR}/status/debug.status
+touch ${REDIS_SERVER_LOGS_DIR}/status/other.status
+
+
+ACTION_FILES_TO_TRACK=$SALES_ADD_LOG_FILE
+ERROR_FILES_TO_TRACK=$SALES_ADD_ERROR_LOG_FILE
+DEBUG_FILES_TO_TRACK=$SERVER_DEBUG_LOG_FILE
+OTHER_FILES_TO_TRACK=$MEMORY_CLEANER_LOG_FILE
+
+mkdir -p ${REDIS_SERVER_LOGS_DIR}
+
+#for each of the files in ACTION_FILES_TO_TRACK
+#check if the file exists
+#if it exists grab the name of the file and the last line of the file
+
+
+
+ACTION_STATUS_FILE="${REDIS_SERVER_LOGS_DIR}/status/action.status"
+ERROR_STATUS_FILE="${REDIS_SERVER_LOGS_DIR}/status/error.status"
+DEBUG_STATUS_FILE="${REDIS_SERVER_LOGS_DIR}/status/debug.status"
+OTHER_STATUS_FILE="${REDIS_SERVER_LOGS_DIR}/status/other.status"
+
+
+echo "----- ACTIONS -----"  > $ACTION_STATUS_FILE
+echo "----- ERRORS  -----"  > $ERROR_STATUS_FILE
+echo "-----  DEBUG  -----"   > $DEBUG_STATUS_FILE
+echo "-----  OTHER  -----"   > $OTHER_STATUS_FILE
+
+
+
 
 while true; do
 
-if [ -f "/server/logs/action/$LAST_LISTINGS_ADD_FILE" ]; then
-    tail -n $NUMBER_OF_LINES "/server/logs/action/$LAST_LISTINGS_ADD_FILE" > /server/logs/status/listings_add.status
-fi
-if [ -f "/server/logs/action/$LAST_LISTINGS_REMOVE_FILE" ]; then
-    tail -n $NUMBER_OF_LINES "/server/logs/action/$LAST_LISTINGS_REMOVE_FILE" > /server/logs/status/listings_remove.status
-fi
-if [ -f "/server/logs/action/$LAST_SALES_ADD_FILE" ]; then
-    tail -n $NUMBER_OF_LINES "/server/logs/action/$LAST_SALES_ADD_FILE" > /server/logs/status/sales_add.status
-fi
-if [ -f "/server/logs/redis_memory_cleaner.log" ]; then
-    tail -n $NUMBER_OF_LINES "/server/logs/redis_memory_cleaner.log" > /server/logs/status/redis_memory_cleaner.status
-fi
+    for file in ${ACTION_FILES_TO_TRACK[@]}; do
+        if [ -f $file ]; then
+            #get just the name of the file
+            file_name=$(basename $file)
+            #replace .log extension in file name with .status extension
+            status_file_name=${file_name%.log}.status
+            LAST_ACTION_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/action/$file_name)
+            LAST_STATUS_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name)
+            if [ "$LAST_ACTION_LINE" != "$LAST_STATUS_LINE" ]; then
+                echo "$LAST_ACTION_LINE" >> ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name
+                echo ["$status_file_name"]"$LAST_ACTION_LINE" >> $ACTION_STATUS_FILE
+            fi            
+        fi
+    done
 
-sleep ${REDIS_STATUS_UPDATER_INTERVAL}
-    
+    for file in ${ERROR_FILES_TO_TRACK[@]}; do
+        if [ -f $file ]; then
+            #get just the name of the file
+            file_name=$(basename $file)
+            #replace .log extension in file name with .status extension
+            status_file_name=${file_name%.log}.status
+            LAST_ERROR_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/error/$file_name)
+            LAST_STATUS_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name)
+            if [ "$LAST_ERROR_LINE" != "$LAST_STATUS_LINE" ]; then
+                echo "$LAST_ERROR_LINE" >> ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name
+                echo ["$status_file_name"]"$LAST_ERROR_LINE" >> $ERROR_STATUS_FILE
+            fi
+        fi
+    done
+
+    for file in ${DEBUG_FILES_TO_TRACK[@]}; do
+        if [ -f $file ]; then
+            #get just the name of the file
+            file_name=$(basename $file)
+            #replace .log extension in file name with .status extension
+            status_file_name=${file_name%.log}.status
+            LAST_DEBUG_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/$file_name)
+            LAST_STATUS_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name)
+            if [ ["$status_file_name"]"$LAST_DEBUG_LINE" != "$LAST_STATUS_LINE" ]; then
+                echo ["$status_file_name"]"$LAST_DEBUG_LINE" >> ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name
+            fi
+        fi
+    done
+
+    for file in ${OTHER_FILES_TO_TRACK[@]}; do
+        if [ -f $file ]; then
+            #get just the name of the file
+            file_name=$(basename $file)
+            #replace .log extension in file name with .status extension
+            status_file_name=${file_name%.log}.status
+            LAST_OTHER_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/$file_name)
+            LAST_STATUS_LINE=$(tail -n 1 ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name)
+            if [ "$LAST_OTHER_LINE" != "$LAST_STATUS_LINE" ]; then
+                echo "$LAST_OTHER_LINE" >> ${REDIS_SERVER_LOGS_DIR}/status/$status_file_name
+                echo ["$status_file_name"]"$LAST_OTHER_LINE" >> $OTHER_STATUS_FILE
+            fi
+        fi
+    done
+
+    sleep ${REDIS_STATUS_UPDATER_INTERVAL}
+
 done
+
+
+
