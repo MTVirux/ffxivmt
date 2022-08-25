4f18e0c6ab6a6367d17c67793f0a14fc934ac60b
MTVirux <supersilver-dark@hotmail.com>
2022-08-25 10:55:37 +0000

    Separated server preparation commands into a script

--- docker/redis/usr/local/bin/startup.sh
+++ docker/redis/usr/local/bin/startup.sh
@@ -2,14 +2,8 @@
 
 /entrypoint.sh &
 
-mkdir -p ${REDIS_DATA_DIR}  # Create data directory if it doesn't exist
-redis-cli config set dir ${REDIS_DATA_DIR} # Set data directory
-redis-cli config set dbfilename $(echo `date +"%Y%m%d%H%M%S"`).rdb #Set the name of the dump file to current timestamp
-redis-cli config set save "" #Disable automatic saving
-redis-cli select 0
-redis-cli set sales ${REDIS_SALES_DB}
-redis-cli set listings ${REDIS_LISTINGS_DB}
-redis-cli set recent ${REDIS_RECENT_DB}
+#Prep redis DB
+/server/redis-config/prep.sh
 
 # Start server process
 /server/workers/run_server.sh &
