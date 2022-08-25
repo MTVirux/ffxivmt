f1708cfe1ffbeb8bdf4ebb551ef43483fef3581a
MTVirux <supersilver-dark@hotmail.com>
2022-08-25 10:54:54 +0000

    Commented out listing threads

--- docker/redis/server/server.py
+++ docker/redis/server/server.py
@@ -27,11 +27,11 @@ import database
 
 threads = []
 
-listing_add_thread              = threading.Thread(target=listings_add.start_listing_add);
-threads.append(listing_add_thread)
+#listing_add_thread              = threading.Thread(target=listings_add.start_listing_add);
+#threads.append(listing_add_thread)
 
-listing_remove_thread           = threading.Thread(target=listings_remove.start_listing_remove);
-threads.append(listing_remove_thread)
+#listing_remove_thread           = threading.Thread(target=listings_remove.start_listing_remove);
+#threads.append(listing_remove_thread)
 
 sales_add_thread                = threading.Thread(target=sales_add.start_sales_add);
 threads.append(sales_add_thread)
