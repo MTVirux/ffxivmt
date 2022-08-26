6f646c11788a53aa98c855a0ad395a21a3e76113
MTVirux <supersilver-dark@hotmail.com>
2022-08-26 16:48:47 +0000

    Changed redis db indexes

--- backend/application/config/redis.php
+++ backend/application/config/redis.php
@@ -23,8 +23,6 @@ $config['redis_timeout'] = 0;
  *      DATABASE INDEXES
  * 
  */
-$config['redis_sales_db'] = 0;
-$config['redis_listings_db'] = 1;
-$config['redis_sales_clean_db'] = 2;
-$config['redis_listings_clean_db'] = 3;
-$config['recent_db'] = 4;
+$config['redis_index_db'] = 0;
+$config['redis_sales_db'] = 1;
+$config['redis_timeseries_db'] = 2;
