996f6db2c3c53fe9ff9f7b0212557b00f12fdccc
MTVirux <supersilver-dark@hotmail.com>
2022-08-26 16:50:20 +0000

    Updated redis model to reflect config changes

--- backend/application/core/MY_Redis_Model.php
+++ backend/application/core/MY_Redis_Model.php
@@ -9,11 +9,14 @@ class MY_Redis_Model extends CI_Model{
     public function __construct() {
         parent::__construct();
         $this->load->config('redis');
+
+        $redis_hosts = $this->config->item('redis_hosts');
+        
         Predis\Autoloader::register();
         $this->redis = new Predis\Client([
-            'scheme' => 'tcp',
-            'host'   => 'ffmt_redis',
-            'port'   => 6379,
+            'scheme' => $redis_hosts['ffmt_redis']['scheme'],
+            'host'   => $redis_hosts['ffmt_redis']['host'],
+            'port'   => $redis_hosts['ffmt_redis']['port']
         ]);
         if($this->redis->ping()){
             return $this->redis;
