e6957b3fbae6c7ead08215b96e6fad45a01acee8
MTVirux <supersilver-dark@hotmail.com>
2022-08-26 16:55:51 +0000

    Added some test cases to get scores

--- backend/application/controllers/Test.php
+++ backend/application/controllers/Test.php
@@ -6,14 +6,27 @@ class Test extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
-
+		$this->load->model('/Redis/Redis_timeseries_model', 'Redis_ts');
 		
 	}
 
-	public function index()
+	public function index($world_name)
 	{
-        var_dump($_POST);
+		$this->Redis_ts->get_world_scores();
+	}
 
+	public function world_scores($world_name="Spriggan"){
+		if(!empty($_GET['world_name']))
+			$world_name = $_GET['world_name'];
+		
+		$this->Redis_ts->get_world_scores($world_name);
+	}
+
+	public function dc_scores($dc_name="Chaos"){
+		if(!empty($_GET['dc_name']))
+			$dc_name = $_GET['dc_name'];
+		
+		$this->Redis_ts->get_dc_scores($dc_name);
 	}
 
 	public function search_item(){
