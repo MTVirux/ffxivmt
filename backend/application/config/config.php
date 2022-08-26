ba391a31314202859cf67322ccf4195134772339
MTVirux <supersilver-dark@hotmail.com>
2022-08-26 16:48:25 +0000

    Removed the config entry for index page as it is default

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://'.(!empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
