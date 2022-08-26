e4cc8c8b67d05575fa28dcb2a8879a678e2e3c71
MTVirux <supersilver-dark@hotmail.com>
2022-08-26 16:52:16 +0000

    Addded item table deletion for update

--- backend/application/models/Item_model.php
+++ backend/application/models/Item_model.php
@@ -6,6 +6,37 @@ class Item_model extends CI_Model {
 		$this->load->database();
 	}
 
+	public function prep_for_update(){
+		$this->db->query('DROP TABLE IF EXISTS items');
+		$this->db->query("CREATE TABLE IF NOT EXISTS `items` (
+			`id` int(11) NOT NULL,
+			`name` varchar(1024) NOT NULL,
+			`description` varchar(2048) DEFAULT NULL,
+			`canBeHQ` tinyint(4) DEFAULT NULL,
+			`alwaysCollectible` varchar(45) DEFAULT NULL,
+			`stackSize` float DEFAULT NULL,
+			`itemLevel` int(11) DEFAULT NULL,
+			`iconImage` int(11) DEFAULT NULL,
+			`rarity` int(11) DEFAULT NULL,
+			`filterGroup` int(11) DEFAULT NULL,
+			`itemUICategory` int(11) DEFAULT NULL,
+			`equipSlotCategory` int(11) DEFAULT NULL,
+			`unique` tinyint(4) DEFAULT NULL,
+			`untradable` tinyint(4) DEFAULT NULL,
+			`dyable` tinyint(4) DEFAULT NULL,
+			`aetherialReductible` tinyint(4) DEFAULT NULL,
+			`materiaSlotCount` int(11) DEFAULT NULL,
+			`itemSearchCategory` int(11) DEFAULT NULL,
+			`disposable` tinyint(4) DEFAULT NULL,
+			`advancedMelding` tinyint(4) DEFAULT NULL
+			)") or die(mysql_error());
+
+			$this->db->query("ALTER TABLE `items`
+			ADD PRIMARY KEY (`id`),
+			ADD UNIQUE KEY `id_UNIQUE` (`id`);
+			") or die(mysql_error());
+	}
+
 	public function add($item)
 	{
 		$this->db->insert('items',$item);
