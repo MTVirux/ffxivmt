59160e0de2f50066934b01c3920e95715533daa0
MTVirux <supersilver-dark@hotmail.com>
2022-09-09 09:49:23 +0000

    Added an usage view for when no function is selected on the test controller

--- backend/application/views/test/usage.php
+++ backend/application/views/test/usage.php
@@ -1,13 +1,16 @@
 <html>
 
     <h1>USAGE:</h1>
+ 
+    <h1><?=base_url('/test/get_world_scores')?>?<b>world_name</b>="Spriggan"</b></h1><br>
+    <b>world_name</b> -> world_name takes the name of any world in the game and preforms a search on all sales data of that world.
+    Test: <a href="<?=base_url('/test/get_world_scores')?>?world_name="Spriggan"></a>
 
-    <?=base_url/test/get_world_scores?>?<b>world_name</b>="Spriggan"</b><br>
-    world_name -> world_name takes the name of any world in the game and preforms a search on all sales data of that world.
-    Test: <a href="<?=base_url/test/get_world_scores?>?world_name="Spriggan"></a>
+</br>
+</br>
 
-    <?=base_url/test/get_world_scores?>?<b>dc_name</b>="Chaos"</b><br>
-    dc_name -> dc_name takes the name of any dc in the game and preforms a search on all sales data of all worlds of that dc.<br>
-    Test: <a href="<?=base_url/test/get_dc_scores?>?dc_name="Chaos"></a>
+    <h1><?=base_url('/test/get_dc_scores')?>?<b>dc_name</b>="Chaos"</b></h1><br>
+    <b>dc_name</b> - dc_name takes the name of any dc in the game and preforms a search on all sales data of all worlds of that dc.<br>
+    Test: <a href="<?=base_url('/test/get_dc_scores')?>?dc_name="Chaos"></a>
 
 </html>
