cdf728bf264d89dc83eb063d4f027e97c16d2162
MTVirux <supersilver-dark@hotmail.com>
2022-09-09 09:52:38 +0000

    Updated database indexing on server startup

--- docker/redis/server/redis-config/prep.sh
+++ docker/redis/server/redis-config/prep.sh
@@ -3,6 +3,7 @@ redis-cli config set dir ${REDIS_DATA_DIR} # Set data directory
 redis-cli config set dbfilename $(echo `date +"%Y%m%d%H%M%S"`).rdb #Set the name of the dump file to current timestamp
 redis-cli config set save "" #Disable automatic saving
 redis-cli select 0
-redis-cli set sales ${REDIS_SALES_DB}
-redis-cli set timeseries ${REDIS_TIMESERIES_DB}
-redis-cli select ${REDIS_TIMESERIES_DB}
+redis-cli set index ${REDIS_INDEX_DB} #Set the index database to the value of REDIS_INDEX_DB
+redis-cli set sales ${REDIS_SALES_DB} #Set the sales database to the value of REDIS_SALES_DB
+redis-cli set timeseries ${REDIS_TIMESERIES_DB} #Set the timeseries database to the value of REDIS_TIMESERIES_DB
+redis-cli select ${REDIS_TIMESERIES_DB} #Select the timeseries database
