ec84da6a9a628619dd16205eb44bfbd1756f00b6
MTVirux <supersilver-dark@hotmail.com>
2022-09-21 15:35:46 +0000

    Added double buffering

--- docker/redis/server/workers/cpu_monitor.sh
+++ docker/redis/server/workers/cpu_monitor.sh
@@ -5,6 +5,7 @@ mkdir -p ${REDIS_SERVER_LOGS_DIR}/system
 touch ${REDIS_SERVER_LOGS_DIR}/system/cpu.log
 
 while true; do
-    mpstat | tail -n 2 > ${REDIS_SERVER_LOGS_DIR}/system/cpu.log
+    TEMP=$(mpstat | tail -n 2)
+    echo "$TEMP" > ${REDIS_SERVER_LOGS_DIR}/system/cpu.log
     sleep $(bc -l <<< "${REDIS_STATUS_UPDATER_INTERVAL}")
 done
