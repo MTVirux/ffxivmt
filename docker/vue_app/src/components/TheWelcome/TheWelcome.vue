86183cbbf7a6dfab7239e8d70a191c3dd71542e8
MTVirux <supersilver-dark@hotmail.com>
2022-09-27 15:55:48 +0000

    Split vues in script, style and ts

---
+++ docker/vue_app/src/components/TheWelcome/TheWelcome.vue
@@ -0,0 +1,3 @@
+<script src="./component.ts"></script>
+<template src="./template.html"></template>
+<style src="./style.scss"></style>
---
+++ docker/vue_app/src/components/TheWelcome/component.ts
@@ -0,0 +1,23 @@
+
+import WelcomeItem from "@/components/WelcomeItem/WelcomeItem.vue";
+import DocumentationIcon from "@/components/icons/IconDocumentation.vue";
+import ToolingIcon from "@/components/icons/IconTooling.vue";
+import EcosystemIcon from "@/components/icons/IconEcosystem.vue";
+import CommunityIcon from "@/components/icons/IconCommunity.vue";
+import SupportIcon from "@/components/icons/IconSupport.vue";
+
+//Export the component
+export default {
+    name: "TheWelcome",
+    components: {
+        WelcomeItem,
+        DocumentationIcon,
+        ToolingIcon,
+        EcosystemIcon,
+        CommunityIcon,
+        SupportIcon
+    }
+};
+
+// Language: typescript
+// Path: docker\vue_app\src\components\TheWelcome\index.ts
