7798e8a6bc581fb20db97fb726176145b6ba3505
MTVirux <supersilver-dark@hotmail.com>
2022-10-13 10:11:02 +0000

    Added a test function to search for a buyer across all sales

---
+++ backend/application/controllers/Test2.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test2 extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+		//$this->load->model('/Redis/Redis_timeseries_model', 'Redis_ts');
+		$this->load->model('/Redis/Redis_sales_model', 'Redis_sales');
+		
+	}
+
+	public function index()
+	{
+		$this->load->view("test/usage");
+	}
+	
+	public function search_buyer($buyer_name){
+		set_time_limit(300);
+		if(empty($buyer_name)){
+			echo "No buyer name provided";
+			return;
+		}
+		$buyer_name = str_replace('_', ' ', $buyer_name);
+		$this->Redis_sales->search_buyer($buyer_name);
+	}
+}
