45c96e0a62d0a2ce0b16a3b7cff9a9e215e291d7
MTVirux <supersilver-dark@hotmail.com>
2022-10-19 10:04:46 +0000

    Disabled all backend logging

--- backend/application/config/logs.php
+++ backend/application/config/logs.php
@@ -3,9 +3,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['custom_log_channels'] = [
-    'ITEM_SCORE',
+    //'ITEM_SCORE',
     //'ITEM_SCORE_2',
-    'PYTHON_UPDATE',
-    'ALL',
-
+    //'PYTHON_UPDATE',
+    //'ALL',
 ];
