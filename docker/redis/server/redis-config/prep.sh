526b4209497ad6eab23ea5d29fa2c39a392efd60
MTVirux <supersilver-dark@hotmail.com>
2022-10-20 09:12:53 +0000

    Removed setting redis config from bash script

--- docker/redis/server/redis-config/prep.sh
+++ docker/redis/server/redis-config/prep.sh
@@ -1,8 +1,4 @@
 mkdir -p ${REDIS_DATA_DIR}  # Create data directory if it doesn't exist
-redis-cli config set dir ${REDIS_DATA_DIR} # Set data directory
-redis-cli config set dbfilename dump.rdb #Set the name of the dump file to current timestamp
-redis-cli config set save "" #Disable automatic saving
-redis-cli config set dir /server/redis-config #Set the config file to the one in the repo
 redis-cli select 0
 redis-cli set index ${REDIS_INDEX_DB} #Set the index database to the value of REDIS_INDEX_DB
 redis-cli set sales ${REDIS_SALES_DB} #Set the sales database to the value of REDIS_SALES_DB
