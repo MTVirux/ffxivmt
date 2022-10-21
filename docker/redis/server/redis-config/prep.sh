2fc06facac85848c61d5358fe7ce208c8bcb24e4
MTVirux <supersilver-dark@hotmail.com>
2022-10-21 10:35:26 +0000

    Flush Listings on server restart

--- docker/redis/server/redis-config/prep.sh
+++ docker/redis/server/redis-config/prep.sh
@@ -1,4 +1,6 @@
 mkdir -p ${REDIS_DATA_DIR}  # Create data directory if it doesn't exist
+redis-cli select 3
+flushdb
 redis-cli select 0
 redis-cli set index ${REDIS_INDEX_DB} #Set the index database to the value of REDIS_INDEX_DB
 redis-cli set sales ${REDIS_SALES_DB} #Set the sales database to the value of REDIS_SALES_DB
