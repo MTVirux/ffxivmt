9aa0dd51c7041e4d145edc8c2e30806724a363df
MTVirux <supersilver-dark@hotmail.com>
2022-11-26 11:39:38 +0000

    Fixed test in the homepage

--- backend/application/views/home.php
+++ backend/application/views/home.php
@@ -3,7 +3,7 @@
 
     <br>    <br>
     <h1> Gil Flux Ranking</h1>
-    Show's the top 10 items that moved the most gil in the past 24 hours for the selected DC/World.
+    Show's the top items that moved the most gil in the past 24 hours for the selected DC/World.
     <br>    <br>
     <h1> Item Product Profit Solver </h1>
     This tool will help you find the best way to make money from a given item.
