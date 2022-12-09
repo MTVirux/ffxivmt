2a501676f00af64034bd7b5bed02ce2aa7f3be31
MTVirux <supersilver-dark@hotmail.com>
2022-12-09 10:23:26 -0100

    Implemented the search buyer world filter on the API

--- backend/application/controllers/api/v1/Search_buyer.php
+++ backend/application/controllers/api/v1/Search_buyer.php
@@ -14,16 +14,20 @@ class Search_buyer extends RestController{
         Header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE'); //method allowed
     }
 
-    public function index_get(){
-        $this->response([
-            'status' => false,
-            'message' => "This endpoint only accepts POST requests",
-        ], 200);
-    }
+    //public function index_get(){
+    //    $this->response([
+    //        'status' => false,
+    //        'message' => "This endpoint only accepts POST requests",
+    //    ], 200);
+    //}
 
     public function index_post(){
         $this->load->model('Redis/Redis_sales_model', 'Redis_sales');
         set_time_limit(300);
+        
+        if(!is_null($_POST['world']) && !empty($_POST['world'])){
+            $world = $_POST['world'];
+        }
         if(empty($_POST['buyer_name'])){
             $this->response([
                 'status' => false,
@@ -31,7 +35,28 @@ class Search_buyer extends RestController{
             ], 400);
         }
         $buyer_name = str_replace('_', ' ', $_POST['buyer_name']);
-        $buyer_history = $this->Redis_sales->search_buyer($buyer_name);
+        $buyer_history = $this->Redis_sales->search_buyer($buyer_name, $world);
+
+        $this->response([
+            'status' => true,
+            'data' => json_encode($buyer_history),
+        ], 200);
+
+    }
+
+    public function index_get($buyer_name, $world){
+        $this->load->model('Redis/Redis_sales_model', 'Redis_sales');
+        set_time_limit(300);
+        if(empty($buyer_name)){
+            $this->response([
+                'status' => false,
+                'message' => "GET request failed, please try again. Missing: buyer_name field",
+            ], 400);
+        }
+        $buyer_name = str_replace('_', ' ', $buyer_name);
+        $buyer_history = $this->Redis_sales->search_buyer($buyer_name, $world);
+
+        pretty_print($buyer_history);die();
 
         $this->response([
             'status' => true,
