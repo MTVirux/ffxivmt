0112c4f0b99e6636f88e829ecf8b69124d0dfa29
MTVirux <supersilver-dark@hotmail.com>
2022-12-12 10:42:16 +0000

    Made garland_db_get_items accept array, single id string and comma separated multiple id string

--- backend/application/helpers/garland_helper.php
+++ backend/application/helpers/garland_helper.php
@@ -3,18 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 function garland_db_get_items($item_id){
 
-    $item_id_array = explode(',', $item_id);
-    //Get the first and last item id
+    $item_id_array = [];
+
+    //Is array
+    if(gettype($item_id) == 'array'){
+        $item_id_array = $item_id
+    if(count(explode(',', $item_id)) > 1){ //String of multiple ids
+        $item_id_array = explode(',' $item_id);
+    }else{ //Is single id String
+        $item_id_array = [];
+        $item_id_array[] = intval($item_id);
+    }
+
+    //Get first and last item id
     $first_item_id = $item_id_array[0];
     $last_item_id = end($item_id_array);
     
+    //Create range string
     if($first_item_id == $last_item_id){
-        $range_string = '[' . $first_item_id . ']';
+        $range_string = $first_item_id;
     }else{
-        $range_string = '[' . $first_item_id . '-' . $last_item_id . ']';
+        $range_string = $first_item_id . '-' . $last_item_id;
     }
 
-    logger("GARLAND_DB", "Getting item data from Garland DB for items: " . $range_string);
+    //Convert $item_id_array to comma separated string
+    $item_id = implode(',', $item_id_array);
+
+    logger("GARLAND_DB", "Getting item data from Garland DB for item(s): " . $range_string);
 
     //GET request to garlandtools.org
     $base_url = 'https://www.garlandtools.org/db/doc/item/en/3/';
