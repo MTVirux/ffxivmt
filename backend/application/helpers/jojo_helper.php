1b5616ed97bd9754e1b56fa051fca767c5921a97
MTVirux <supersilver-dark@hotmail.com>
2022-12-15 14:22:34 +0000

    Separate log files by channel and files are now not timecoded

--- backend/application/helpers/jojo_helper.php
+++ backend/application/helpers/jojo_helper.php
@@ -18,18 +18,11 @@ function logger($channel, $message, $custom_file = null){
     if(!in_array($channel, config_item('custom_log_channels')) && !in_array('ALL', config_item('custom_log_channels'))){
         return;
     }
-    
-    //open or create file in log folder
-    if(is_null($custom_file)){
-        
-        $file = fopen(APPPATH . 'logs/' . 'log' . '-' . date('Y-m-d') .'.log', 'a');
 
-    }else{
-        $file = fopen(APPPATH . 'logs/' . $custom_file . '-' . date('Y-m-d') .'.log', 'a');
-    }
+    $file = fopen(APPPATH . 'logs/' . $channel . '.log', 'a');
     
     //write message to file
-    fwrite($file, $channel . ' - ' . $message . PHP_EOL);
+    fwrite($file, $message . PHP_EOL);
 
     //close file
     fclose($file);
