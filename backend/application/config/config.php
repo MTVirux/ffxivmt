2af09fd8ccdaeb002b5c70f6102a48e3f9442289
MTVirux <supersilver-dark@hotmail.com>
2022-12-16 09:44:37 -0100

    Default to http if no HTTP_HOST

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://'.(!empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'mtvirux.app');
+$config['base_url'] = (!empty($_SERVER['HTTP_HOST']) ? 'http://'.$_SERVER['HTTP_HOST'] : 'https://'.'mtvirux.app');
 
 /*
 |--------------------------------------------------------------------------
