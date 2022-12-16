9159a6df140acd17fb958d226abd12bfef3268cf
MTVirux <supersilver-dark@hotmail.com>
2022-12-16 10:28:38 -0100

    Restructured script swapping

--- swap_scripts.sh
+++ swap_scripts.sh
@@ -1,71 +1,16 @@
 #!/bin/bash
-ENV=0;
-BAT_IN_SCRIPTS=0;
-SH_IN_SCRIPTS=0;
 
-chmod +x scripts/*.sh
+echo "Press 1 for Linux"
+echo "Press 2 for Windows"
+echo "Press 3 for PowerShell"
+read choice
 
-for file in ./scripts/*; do
-    if [ -d "$file" ]; 
-    then
-        echo "Ignoring directory: $file"
-    elif [ -f "$file" ];
-    then
-        if [[ $file == *.bat ]]
-        then
-                BAT_IN_SCRIPTS=1;
-        elif [[ $file == *.sh ]];
-        then
-                SH_IN_SCRIPTS=1;
-        fi
-    fi
-done
-
-if [ $BAT_IN_SCRIPTS = 1 ] && [ $SH_IN_SCRIPTS = 0 ];
-then 
-        for bat_script in ./scripts/*; do
-                if [ -f "$bat_script" ] && [[ $bat_script == *.bat ]];
-                then
-                        
-                        file_no_path=${bat_script##*/}
-                        filename=${file_no_path%%.*}
-
-                        mv ./scripts/"${filename}".bat .
-                        mv ./"${filename}".sh ./scripts/
-
-                        echo "Swapping ./scripts/${filename}.bat with ./${filename}.sh"
-                        
-                fi
-        done
-        echo "Swapped to Windows .bat scripts"
-elif [ $BAT_IN_SCRIPTS = 0 ] && [ $SH_IN_SCRIPTS = 1 ];
-then
-        for shell_script in ./scripts/*; do
-                if [ -f "$shell_script" ] && [[ $shell_script == *.sh ]];
-                then
-                        
-                        file_no_path=${shell_script##*/}
-                        filename=${file_no_path%%.*}
-                        
-                        mv ./scripts/"${filename}".sh .
-                        mv ./"${filename}".bat ./scripts/
-                        
-                        echo "Swapping ./scripts/${filename}.sh with ./${filename}.bat"
-                fi
-        done
-        echo "Swapped to BASH scripts"
-elif [ $BAT_IN_SCRIPTS = 1 ] && [ $SH_IN_SCRIPTS = 1 ];
-then
-        for shell_script in ./scripts/*; do
-                if [ -f "$shell_script" ] && [[ $shell_script == *.sh ]];
-                then
-                        
-                        file_no_path=${shell_script##*/}
-                        filename=${file_no_path%%.*}
-                        
-                        mv ./scripts/"${filename}".sh .                        
-                fi
-        done
-        echo "Swapping ./scripts/${filename}.sh with ./${filename}.bat"
-        echo "Moved BASH scripts to ."
+if [ $choice -eq 1 ]; then
+  cp ./scripts/sh/* .
+elif [ $choice -eq 2 ]; then
+  cp ./scripts/bat/* .
+elif [ $choice -eq 3 ]; then
+  cp ./scripts/ps/* .
+else
+  echo "Invalid selection"
 fi
