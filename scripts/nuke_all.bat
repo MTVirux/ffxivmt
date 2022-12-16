5b5b2399c6ab4fdd3cb977ce96628e8b00476421
MTVirux <supersilver-dark@hotmail.com>
2022-12-16 09:18:41 -0100

    Fixed bat nuke all script

--- scripts/nuke_all.bat
+++ scripts/nuke_all.bat
@@ -2,14 +2,12 @@
 
 docker compose down
 
-docker volume rm $(docker volume ls -q)
-
-docker images -a -q | % { docker image rm $_ -f }
+for /f "tokens=*" %%i in ('docker images -q') do (
+    docker rmi %%i
+)
 
 docker volume rm ffmt_mariadb_data
 docker volume rm ffmt_redis_data
-docker volume rm ffmt_redisinsight_data
 
 docker volume create ffmt_mariadb_data
 docker volume create ffmt_redis_data
-docker volume create ffmt_redisinsight_data
