71c73fab5e91a013c23eebf4d4eec97e16ef516c
MTVirux <supersilver-dark@hotmail.com>
2022-12-21 14:51:34 +0000

    Updated the log storage script to grab files from ws_worker instead of redis

--- scripts/cron/store_logs.sh
+++ scripts/cron/store_logs.sh
@@ -6,18 +6,18 @@ echo "$(date +%Y-%m-%d) @ $(date +%H:%M:%S) - Cron task was run to backup and zi
 mkdir -p /root/logs
 mkdir -p /root/logs/temp
 mkdir -p /root/logs/temp/backend
-mkdir -p /root/logs/temp/redis
-mkdir -p /root/logs/temp/redis/action
-mkdir -p /root/logs/temp/redis/error
-mkdir -p /root/logs/temp/redis/debug
+mkdir -p /root/logs/temp/ws_worker
+mkdir -p /root/logs/temp/ws_worker/action
+mkdir -p /root/logs/temp/ws_worker/error
+mkdir -p /root/logs/temp/ws_worker/debug
 
 
 # Copy logs to backup folder
 echo "$(date +%Y-%m-%d) @ $(date +%H:%M:%S) - Moving logs to /root/logs/temp..." >> /root/logs/cron.log
 mv /root/ffxiv-market-tools/backend/application/logs/*.log /root/logs/temp/backend
-mv /root/ffxiv-market-tools/docker/redis/server/logs/action/*.log /root/logs/temp/redis/action
-mv /root/ffxiv-market-tools/docker/redis/server/logs/error/*.log /root/logs/temp/redis/error
-mv /root/ffxiv-market-tools/docker/redis/server/logs/debug/*.log /root/logs/temp/redis/debug
+mv /root/ffxiv-market-tools/docker/ws_worker/server/logs/action/*.log /root/logs/temp/ws_worker/action
+mv /root/ffxiv-market-tools/docker/ws_worker/server/logs/error/*.log /root/logs/temp/ws_worker/error
+mv /root/ffxiv-market-tools/docker/ws_worker/server/logs/debug/*.log /root/logs/temp/ws_worker/debug
 
 #zip the temp folder into a timestamped zip file
 zip -r /root/logs/$(date +%Y-%m-%d_%H-%M-%S).zip /root/logs/temp >> /root/logs/cron.log
