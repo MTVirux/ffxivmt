cb807d588519d18c82340ea527ac5e0e2f17522b
MTVirux <supersilver-dark@hotmail.com>
2022-12-21 15:37:09 +0000

    Updated RefreshDB sh script

--- scripts/sh/refreshDB.sh
+++ scripts/sh/refreshDB.sh
@@ -1,6 +1,11 @@
-curl -X POST mtvirux.app/updatedb/no
-echo 1
-curl -X POST mtvirux.app/test/update_item_scores
-echo 2
-curl -X POST mtvirux.app/test/transpose_sales_to_ts
-echo 3
+echo "Updating Item DB from CSV (Log Channel ITEM_DB)..."
+curl -X POST localhost/updatedb/ > /dev/null
+
+echo "Updating item sales from universalis (Log Channel UNIVERSALIS_API)..."
+curl -X POST localhost/updatedb/update_sales_from_universalis > /dev/null
+
+echo "Transposing sales to timeseries..."
+curl -X POST localhost/test/transpose_sales_to_ts > /dev/null
+
+echo "Updating item scores..."
+curl -X POST localhost/test/update_item_scores > /dev/null
