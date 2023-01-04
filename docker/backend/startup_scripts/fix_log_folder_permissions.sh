17c48f9cf5df9f0df812f9e4c1a21ce1f4456e92
MTVirux <supersilver-dark@hotmail.com>
2023-01-04 12:15:17 +0000

    Create log folder if it's not there for some reason

--- docker/backend/startup_scripts/fix_log_folder_permissions.sh
+++ docker/backend/startup_scripts/fix_log_folder_permissions.sh
@@ -3,5 +3,6 @@
 # This script fixes the permissions of the log folder so that the
 # backend can write to it.
 
+mkdir -p /var/www/html/application/logs
 chmod -R 777 /var/www/html/application/logs
 
