564cceebada896d15a3ac221bbfa5e7d0cb69e98
MTVirux <supersilver-dark@hotmail.com>
2023-01-04 10:46:36 +0000

    Updated first start script

--- scripts/sh/first_start.sh
+++ scripts/sh/first_start.sh
@@ -1,29 +1,13 @@
+rm -rf ./docker/scylla/persistent
+
 echo "Spinning up Scylla..."
 docker-compose up -d ffmt_scylla
-
-echo "Spinning up MariaDB..."
-docker-compose up -d ffmt_mariadb
-
-echo "Import MariaDB table backups..."
-docker exec -it ffmt_mariadb /bin/bash import_maria_db.sh
 
 echo "Spinning up PHP backend..."
 docker-compose up -d ffmt_backend
 
-echo "Spinning up WS Worker..."
-docker-compose up -d ffmt_scylla
-
-echo "Spinning up PMA..."
-docker-compose up -d ffmt_pma
-
 echo "Updating Item DB from CSV (Log Channel ITEM_DB)..."
 curl -X POST localhost/updatedb/ > /dev/null
 
-echo "Updating item sales from universalis (Log Channel UNIVERSALIS_API)..."
-curl -X POST localhost/updatedb/update_sales_from_universalis > /dev/null
-
-echo "Transposing sales to timeseries..."
-curl -X POST localhost/test/transpose_sales_to_ts > /dev/null
-
-echo "Updating item scores..."
-curl -X POST localhost/test/update_item_scores > /dev/null
+echo "Spinning up WS Worker..."
+docker-compose up -d ffmt_ws_worker
