172884d3503dd1406c5a5e62494aa8249f5e8b65
MTVirux <supersilver-dark@hotmail.com>
2023-01-04 10:18:10 +0000

    Specified scylla keyspace

--- docker/ws_worker/server/database.py
+++ docker/ws_worker/server/database.py
@@ -1,8 +1,4 @@
 from cassandra.cluster import Cluster
 
 SCYLLA_DB_CLUSTER = Cluster(['ffmt_scylla'])
-SCYLLA_DB = SCYLLA_DB_CLUSTER.connect('sales')
-
-# Check if the connection is working by executing a simple query
-result = SCYLLA_DB.execute("SELECT release_version FROM system.local")
-print(result.one())
+SCYLLA_DB = SCYLLA_DB_CLUSTER.connect('ffmt')
