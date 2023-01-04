9f3e0de920e9d49cd4e251809217c028a0179463
MTVirux <supersilver-dark@hotmail.com>
2023-01-04 10:14:04 +0000

    Created function to update scylla DBs

--- backend/application/controllers/Updatedb.php
+++ backend/application/controllers/Updatedb.php
@@ -5,27 +5,46 @@ class Updatedb extends MY_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('Item_model', 'Items');
 		$this->load->helper('url');
-		ini_set('max_execution_time', 9000);
+		ini_set('max_execution_time', 0);
 		ini_set('memory_limit', '2048M');
 	}
 
 
 	public function index(){
 		$this->update_items();
-		$this->update_craft_recipes_from_garland_db();
+		$this->update_craft_recipes(true, 0, 999999999);
+		$this->update_marketability();
+		$this->update_worlds();
+		$this->update_sales();
+	}
+
+	public function update_marketability(){
+		$this->load->model('Scylla/Item_model', 'Scylla_items');
+		$marketable_items_ids = universalis_get_marketable_item_ids();
+
+		$marketable_item_count = count($marketable_items_ids);
+		$updated_items = 0;
+
+		foreach($marketable_items_ids as $marketable_item_id){
+
+			if($this->Scylla_items->update(array("id" => $marketable_item_id, "marketable" => true))){
+				$updated_items++;
+				logger("SCYLLA_DB" , json_encode(array("message" => "Item marketability updated", "item_id" => $marketable_item_id)));
+			}else{	
+				logger("SCYLLA_DB" , json_encode(array("message" => "[ERROR] Failed to update item marketability", "item_id" => $marketable_item_id)));
+				die();
+			}
+		}
+
+		logger("SCYLLA_DB" , json_encode(array("message" => "Item marketability updated", "updated_items" => $updated_items, "total_items" => $marketable_item_count)));
+
 	}
 
 	
-	public function update_items()
-	{
+	public function update_items(){
 
 		$csv = $this->parse_csv();
-
-		logger("ITEM_DB", "Prepping DB for update: " . count($csv) . " items");
-		$this->Items->prep_for_update();
-		logger("ITEM_DB", "DB ready to recieve update");
 
 		foreach($csv as $item){
 
@@ -33,227 +52,140 @@ class Updatedb extends MY_Controller {
 
 			//pretty_dump($organized_item);die();
 
-			if(!isset($organized_item['name'])){
-				var_dump("Culprit: " . $organized_item['id']);
-				logger("ITEM_DB_UPDATE", "Culprit: " . $organized_item['id'], "error");
+			if(!isset($item['name'])){
+				var_dump("Culprit: " . $item['id']);
+				logger("SCYLLA_DB", json_encode(array("message" => "Culprit: " . $item['id'], "error")));
 				pretty_dump($item);
 				die();
 			}
 			
-			if($this->Items->add($organized_item)){
-				logger("ITEM_DB", json_encode(array("job"=>"Item Entry Update", "item_id" => $organized_item["id"], "item_name" => $organized_item["name"])));
+			$this->load->model('Scylla/Item_model', 'Scylla_items');
+			if($this->Scylla_items->add($item)){
+				logger("SCYLLA_DB" , json_encode(array("message" => "Item added to database", "item_id" => $item["id"], "item_name" => $item['name'])));
+			}else{
+				logger("SCYLLA_DB" , json_encode(array("message" => "[ERROR] Failed to add item to DB", "item_id" => $item["id"])));
+				die();
 			}
 		}
-
-		logger("ITEM_DB", json_encode(array("job" => "Item Entry Update [DONE]")));
 	}
 
-	public function verify_item_entries_against_garland_db(){
+	public function update_craft_recipes(){
 
-		//Get an array with numbers from 1 to 39000
-		$all_item_ids = range(0, 39000);
-
-		foreach($all_item_ids as $item_id){
-
-			if(in_array($item_id, $this->config->item('preapproved_item_ids'))){
-				logger("ITEM_DB", json_encode(array("job" => "Item DB Verification", "message"=>"Skipping item: " . $item_id . " - Preaproved")));
-				continue;
-			}
-
-			$local_item_data = $this->Items->get($item_id);
-
-			//If local item data name includes Dated, then it's a dated item and we don't need to verify it
-			if(strpos($local_item_data->name, 'Dated') !== false){
-				logger("ITEM_DB", json_encode(array("job" => "Item DB Verification", "message"=>"Skipping item: " . $item_id . " - Because it is a dated item")));
-				continue;
-			}
-			
-			//If local item data has no name, then it's an unused item slot and we don't need to verify it
-			if($local_item_data->name == ""){
-				logger("ITEM_DB", json_encode(array("job" => "Item DB Verification", "message"=>"Skipping item: " . $item_id . " - Because it is an unused item slot")));
-				continue;
-			}
-
-			$garland_item_data = garland_db_get_items($item_id);
-
-			//Ignore items with no entry in Garland DB
-			if($garland_item_data === False && !empty($local_item_data->name)){
-				logger("ITEM_DB_VERIFICATION", "Item: " . $item_id . " has no entry in Garland DB. Skipping.");
-				logger("ITEM_DB_VERIFICATION", "Local was name " . $local_item_data->name);
-				logger("ITEM_DB_VERIFICATION", "DID LOCAL EXIST: " . !empty($local_item_data->name));
-				//pretty_dump($garland_item_data);
-				pretty_dump($local_item_data);
-				die();
-			}
-			
-
-			//Adjust garland item data
-			$garland_item_data["item"]["icon"] = str_replace("t/", "", $garland_item_data["item"]["icon"]);
-
-
-
-			if($garland_item_data["item"]["name"] != $local_item_data->name){
-				logger("[ERROR]ITEM_DB", "Name:" . $garland_item_data["item"]["name"] . " != " . $local_item_data["name"] . " for item id: " . $item_id);
-				pretty_dump($garland_item_data);
-				pretty_dump($local_item_data);
-				die();
-			}
-			//if($garland_item_data["item"]["description"] != $local_item_data->description){
-			//	logger("[ERROR]ITEM_DB", "Description:" . $garland_item_data["item"]["description"] . " != " . $local_item_data->description . " for item id: " . $item_id);
-			//	pretty_dump($garland_item_data);
-			//	pretty_dump($local_item_data);
-			//	die();
-			//}
-
-			if(intval($garland_item_data["item"]["icon"]) != intval($local_item_data->iconImage)){
-				logger("[ERROR]ITEM_DB", "Icon:" . $garland_item_data["item"]["icon"] . " != " . $local_item_data->iconImage . " for item id: " . $item_id);
-				pretty_dump($garland_item_data);
-				pretty_dump($local_item_data);
-				die();
-			}
-
-			logger("ITEM_DB", json_encode(array("job" => "Item DB Verification", "item_id" => $item_id, "status" => 1, "message" => "SUCCESS")));
-		}
-
-		logger("ITEM_DB", json_encode(array("job" => "Item DB Verification[DONE]")));
-
-	}
-
-	public function update_craft_recipes_from_garland_db(){
-
-		$all_ids = $this->Items->get_all_ids();
+		$this->load->model('Scylla/Item_model', 'Scylla_items');
+		$all_ids = $this->Scylla_items->get_all_ids();
 		$ids_to_request = array();
+
+		$total_items = 0;
+		$items_with_crafting = 0;
 		
 		foreach($all_ids as $id){
-			$ids_to_request[] = $id->id;
-			if(count($ids_to_request) == 100 || $id->id == 39000){
+
+			$total_items++;
+
+			$ids_to_request[] = $id;
+			if(count($ids_to_request) == 100 || $id == 39000){
+
 				$json = file_get_contents('https://www.garlandtools.org/db/doc/item/en/3/' . implode(',', $ids_to_request). '.json');
 				$json_decoded = json_decode($json, true);
 				$ids_to_request = array();
+
 				foreach($json_decoded as $item){
 					if(isset($item["obj"]["item"]["craft"])){
+
+						$items_with_crafting++;
+
 						$craftingComplexity = array();
-						$current_item = $this->Items->get($item["id"]);
-						$current_item->craftingRecipe = json_encode($item["obj"]["item"]["craft"]);
+						$current_item = $this->Scylla_items->get($item["id"])[0];
+
 						foreach($item["obj"]["item"]["craft"] as $key=>$recipe){
-							$craftingComplexity[$key] = json_encode($recipe["complexity"]);
+							$craftingComplexity[$key] = $recipe["complexity"];
 						};
-						$current_item->craftingComplexity = json_encode($craftingComplexity);
-						$new_item = $this->Items->update($current_item);
-						logger("ITEM_DB", json_encode(array("job"=>"GarlandDB Craft Update", "item_id" => $item["id"], "craft_complexity" => $recipe["complexity"])));
+
+						unset($item["obj"]["item"]["craft"][0]["complexity"]);
+						$current_item["crafted"]	=	!empty($item["obj"]["item"]["craft"][0]) ? true : false;
+
+						if($this->Scylla_items->update($current_item)){
+							logger("SCYLLA_DB" , "Item recipe updated: " . $current_item["id"] . ' - ' . $current_item['name']);
+						}
 					}
 				}
 			}
 		}
+
+		logger("SCYLLA_DB" , json_encode(array("message" => "Item craftability updated", "craftable_items" => $items_with_crafting)));
+
 	}
 
-	public function update_sales_from_universalis($reverse = false, $start_at_id= null, $end_at_id = null){
+	public function update_sales($reverse = true, $start_at_id= 0, $end_at_id = 999999999){
 
-		$this->load->model('Redis/Redis_sales_model', 'Redis_sales');
+		$this->load->model('Scylla/Sale_model', 'Sale_model');
+		$this->load->model('Scylla/World_model', 'World_model');
+		$this->load->model('Scylla/Item_model', 'Item_model');
+
+		$item_names = $this->Item_model->get_name();
 
 		//Get marketable items from universalis
-		$marketable_items = universalis_get_marketable_item_ids();
-
-		//Remove all between the values of start and end ids
-		if($start_at_id != null && $end_at_id != null){
-			foreach($marketable_items as $key=>$item_id){
-				if($item_id < $start_at_id){
-					unset($marketable_items[$key]);
-				}
-				if($item_id > $end_at_id){
-					unset($marketable_items[$key]);
-				}
+		$marketable_items = boolval($reverse) ? array_reverse(universalis_get_marketable_item_ids()) : universalis_get_marketable_item_ids();
+		foreach($marketable_items as $key=>$item){
+			if($item < $start_at_id || $item > $end_at_id){
+				unset($marketable_items[$key]);
 			}
-		}
-
-		//Reverse if requested
-		if(is_null($reverse) || $reverse == true){
-			$marketable_items = array_reverse($marketable_items);
 		}
 
 		
 		//Split into chunks of 100
-		$chunks = array_chunk($marketable_items, 50);
+		$chunks = array_chunk($marketable_items, 10);
 		
 		//Make each chunk a string separated by commas
 		$chunks = array_map(function($chunk){
 			return implode(',', $chunk);
 		}, $chunks);
 
-		$worlds_to_use = $this->config->item('worlds_to_use');
-		$regions_to_use = $this->config->item('regions_to_use');
-		$dcs_to_use = $this->config->item('dcs_to_use');
-
-		$total_count_requests = (count($chunks) * count($worlds_to_use)) + (count($chunks) * count($regions_to_use)) + (count($chunks) * count($dcs_to_use));
-		$count_requests = 0;
+		$regions = $this->World_model->get_regions();
 
 		$total_sales_entries_fulfilled = 0;
+		$requests_fullfilled = 0;
+		$total_request_count = count($chunks) * count($regions);
 
 		foreach($chunks as $chunk){
-			//first and last id string
-			$first_id = explode(',', $chunk)[0];
-			$last_id = explode(',', $chunk)[count(explode(',', $chunk)) - 1];
-			$id_range = '[' . $first_id . '-' . $last_id . ']';
-			$consolidated_sales_data = array();
 
+			foreach($regions as $region){
+				$consolidated_sales_data = array();
 
-			//Handle Region requests and consolidate data
-			foreach($regions_to_use as $region_to_use){
-				$sales_data = universalis_get_item_sales_data($chunk, $region_to_use);
-				$count_requests++;
-				
-				logger("UNIVERSALIS_API", "Parsing sales data for " . $id_range . " @ " . $region_to_use);
-				foreach($sales_data["items"] as $item_id => $item_entry){
-					foreach($item_entry["entries"] as $sale_entry){
-						$sale_entry["itemID"] = $item_id;
-						$sale_entry["total"] = $sale_entry["quantity"] * $sale_entry["pricePerUnit"];
-						$consolidated_sales_data[] = $sale_entry;
-					}
-				}
-				sleep(0.1);
-			}
+				$sales_data = universalis_get_item_sales_data($chunk, $region);
+				$requests_fullfilled++;
 
-			//Handle Datacenter requests and consolidate sales data
-			foreach($dcs_to_use as $dc_to_use){
-				$sales_data = universalis_get_item_sales_data($chunk, $dc_to_use);
-				$count_requests++;
-				foreach($sales_data["items"] as $item_id => $item_entry){
-					foreach($item_entry["entries"] as $sale_entry){
-						
-						$sale_entry["itemID"] = $item_id;
-						$sale_entry["total"] = $sale_entry["quantity"] * $sale_entry["pricePerUnit"];
-						$consolidated_sales_data[] = $sale_entry;
+				foreach($sales_data["items"] as $sales){
+
+					foreach($sales["entries"] as $sale_entry){
+
+						$consolidated_sales_data[] = array(
+							"hq" => $sale_entry["hq"],
+							"unit_price" => $sale_entry["pricePerUnit"],
+							"quantity" => $sale_entry["quantity"],
+							"buyer_name" => $sale_entry["buyerName"],
+							"on_mannequin" => $sale_entry["onMannequin"],
+							"sale_time" => $sale_entry["timestamp"],
+							"world_name" => $sale_entry["worldName"],
+							"world_id" => $sale_entry["worldID"],
+							"total" => $sale_entry["pricePerUnit"] * $sale_entry["quantity"],
+							"item_id" => $sales["itemID"],
+							"item_name" => $item_names[$sales["itemID"]]
+						);
 
 					}
+
+
 				}
-				sleep(0.1);
+
+				$this->Sale_model->add_sale($consolidated_sales_data);
+			
+				logger("UNIVERSALIS_API", "Total fulfilled updated sales requests: " . $requests_fullfilled . "/" . $total_request_count);
+				$total_sales_entries_fulfilled = $total_sales_entries_fulfilled + count($consolidated_sales_data);
+				logger("UNIVERSALIS_API", "Completed " . $requests_fullfilled . " of " . $total_request_count . " requests  currently @ " . $total_sales_entries_fulfilled . " sales entries");
+
 			}
 
-			//Handle World Request and consolidate sales data
-			foreach($worlds_to_use as $world_to_use){
-				$sales_data = universalis_get_item_sales_data($chunk, $world_to_use);
-				$count_requests++;
-				foreach($sales_data["items"] as $item_id => $item_entry){
-
-					$worldID = $item_entry["worldID"];
-					$worldName = get_world_name($worldID, $this->config->item('ffxiv_worlds'));
-					foreach($item_entry["entries"] as $sale_entry){
-						$sale_entry["worldID"] = $worldID;
-						$sale_entry["worldName"] = $worldName;
-						$sale_entry["total"] = $sale_entry["quantity"] * $sale_entry["pricePerUnit"];
-						$sale_entry["itemID"] = $item_id;
-
-						$consolidated_sales_data[] = $sale_entry;
-					}
-				}
-				sleep(0.1);
-			}
-
-			$this->Redis_sales->add_sale($consolidated_sales_data);
-			logger("UNIVERSALIS_API", "Total fulfilled updated sales requests: " . $count_requests . "/" . $total_count_requests);
-			$total_sales_entries_fulfilled = $total_sales_entries_fulfilled + count($consolidated_sales_data);
-			logger("UNIVERSALIS_API", "Completed " . $count_requests . " of " . $total_count_requests . " requests  currently @ " . $total_sales_entries_fulfilled . " sales entries");
-			$consolidated_sales_data = array();
 		}
 
 		return true;
@@ -299,26 +231,28 @@ class Updatedb extends MY_Controller {
 			$element_offset = 1;
 
 			$item_data = array(
-				"id" => 					intval(		$item[array_key_first($item)]	),
-				"name" => 								$item[10-$element_offset],
-				"description" => 						$item[9-$element_offset],
-				"canBeHQ" => 				boolval(	$item[28-$element_offset]						),
-				"alwaysCollectible" => 					$item[39-$element_offset],
-				"stackSize" => 				intval(		$item[21-$element_offset]						),
-				"itemLevel" => 				intval(		$item[12-$element_offset]						),
-				"iconImage" => 				intval(		$item[11-$element_offset]						),
-				"rarity" => 				boolval(	$item[13-$element_offset]						),
-				"filterGroup" => 			boolval(	$item[14-$element_offset]						),
-				"itemUICategory" => 		boolval(	$item[16-$element_offset]						),
-				"itemSearchCategory" => 	boolval(	$item[17-$element_offset]						),
-				"equipSlotCategory" => 		boolval(	$item[18-$element_offset]						),
-				"unique" => 				boolval(	$item[22-$element_offset]						),
-				"untradable" => 						$item[23-$element_offset],
-				"disposable" => 			boolval(	$item[24-$element_offset]						),
-				"dyable" => 				boolval(	$item[29-$element_offset]						),
-				"aetherialReductible" =>	boolval(	$item[40-$element_offset]						),
-				"materiaSlotCount" => 		boolval(	$item[87-$element_offset]						),
-				"advancedMelding" => 		boolval(	$item[88-$element_offset]						),
+				'id'                    =>	intval(		$item[array_key_first($item)]	),
+				'name'                  =>	strval(		$item[10-$element_offset]		),
+				'description'           =>	strval(		$item[9-$element_offset]		),
+				'can_be_hq'             =>	boolval(	$item[28-$element_offset]		),
+				'always_collectible'    =>	strval(		$item[39-$element_offset]		) == "True" ? true : false,
+				'stack_size'            =>	intval(		$item[21-$element_offset]		),
+				'item_level'            =>	intval(		$item[12-$element_offset]		),
+				'icon_image'            =>	intval(		$item[11-$element_offset]		),
+				'rarity'                =>	intval(		$item[13-$element_offset]		),
+				'filter_group'          =>	intval(		$item[14-$element_offset]		),
+				'item_ui_category'      =>	intval(		$item[16-$element_offset]		),
+				'item_search_category'  =>	intval(		$item[17-$element_offset]		),
+				'equip_slot_category'   =>	intval(		$item[18-$element_offset]		),
+				'unique'                =>	boolval(	$item[22-$element_offset]		),
+				'untradable'            =>	boolval(	$item[23-$element_offset]		),
+				'disposable'            =>	boolval(	$item[24-$element_offset]		),
+				'dyable'                =>	boolval(	$item[29-$element_offset]		),
+				'aetherial_reductible'  =>	boolval(	$item[40-$element_offset]		),
+				'materia_slot_count'    =>	intval(		$item[87-$element_offset]		),
+				'advanced_melding'      =>	boolval(	$item[88-$element_offset]		),
+				'crafted'       		=> 	false, //Fields got from Garland DB just here to appease the php-cql lib gods
+				'marketable'       		=> 	false, //Fields got from Garland DB just here to appease the php-cql lib gods
 			);
 		
 			// Print the data for this item for debugging purposes
@@ -331,5 +265,23 @@ class Updatedb extends MY_Controller {
 
 		return $final_item_data;
 		
+	}
+
+	public function update_worlds(){
+
+		$this->load->model('Scylla/World_model', 'Scylla_worlds');
+		$worlds = universalis_get_all_worlds();
+
+		$total_worlds = count($worlds);
+		$worlds_added = 0;
+
+		foreach($worlds as $world){
+			if($this->Scylla_worlds->add($world)){
+				$worlds_added++;
+			}
+		}
+
+		logger("SCYLLA_DB", json_encode(array("message" => "Added " . $worlds_added . " of " . $total_worlds . " worlds")));
+
 	}
 }
