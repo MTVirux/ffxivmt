5c2231c45dec3c11da3ea839c5240cca12385da6
MTVirux <supersilver-dark@hotmail.com>
2023-01-07 01:06:37 +0000

    Added scripts to worker to create  and set permissions for log folders

---
+++ docker/ws_worker/startup_scripts/2- create_logs_for_sales_importer.sh
@@ -0,0 +1,6 @@
+mkdir -p /sales_importer/logs/action
+mkdir -p /sales_importer/logs/error
+mkdir -p /sales_importer/logs/debug
+mkdir -p /sales_importer/logs/panic
+
+chmod 777 -R /sales_importer/logs
