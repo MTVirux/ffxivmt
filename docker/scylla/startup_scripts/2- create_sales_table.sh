6b4b8a21672b3072cc5fb67e57cc903de81405d0
MTVirux <supersilver-dark@hotmail.com>
2023-01-07 01:12:02 +0000

    Updated sales primary key to include buyer_name

--- docker/scylla/startup_scripts/2- create_sales_table.sh
+++ docker/scylla/startup_scripts/2- create_sales_table.sh
@@ -11,6 +11,6 @@ cqlsh -e "CREATE TABLE IF NOT EXISTS ffmt.sales (buyer_name text,
  world_name text,
  item_name text,
  total int,
- PRIMARY KEY ((item_id,world_id),sale_time))"
+ PRIMARY KEY ((item_id,world_id),sale_time, buyer_name))"
 #Secondary index the buyer_name column
 cqlsh -e "CREATE INDEX IF NOT EXISTS buyers ON ffmt.sales (buyer_name)"
