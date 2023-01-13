993cdfd8afefb56a20cdae8bcde12ba7e1a2f111
MTVirux <supersilver-dark@hotmail.com>
2023-01-13 17:12:25 +0000

    Added some parameters to config

--- docker/ws_worker/sales_importer/python/config.py
+++ docker/ws_worker/sales_importer/python/config.py
@@ -1,11 +1,24 @@
 import os
 
+#API CONFIG
+
+ENTRIES_TO_RETURN = 999999                                          #Max number of entries to return (Max: 999999)
+
+IMPORT_ALL_TIME = True                                              #Set to true to import all time, false to import the time set in TIME_TO_IMPORT_SALES
+
+TIME_AGO_TO_IMPORT_SALES = 7776000000                               #Time in milliseconds to import sales for, only used if IMPORT_ALL_TIME is false
+
+UNIVERSALIS_URL = "https://universalis.app/api/v2/"
+UNIVERSALIS_SALES_ENDPOINT = "history/"
+
+
+
 #LOGGING
 
-LOGS_DIR = "/sales_importer/logs/"
+LOGS_DIR = "/sales_importer/logs/" #Directory to store logs in
 
-#Should server print to logs
-PRINT_TO_LOG = { 
+#What channels should server print to logs
+PRINT_TO_LOG = {
 "DEBUG":False,
 "ERROR":True,
 "ACTION":False,
@@ -25,10 +38,6 @@ PRINT_TO_SCREEN = {
 "ERROR":True,
 "ACTION":False,
 }
-
-### UNIVERSALLIS CONFIG
-
-UNIVERSALLIS_URL = "https://universalis.app/api/v2/"
 
 
 ### EXTERNAL CONTAINERS
