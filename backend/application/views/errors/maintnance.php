edcbb3b1329ed20df5f6cd46063d91b66f734897
MTVirux <supersilver-dark@hotmail.com>
2023-02-06 16:29:24 +0000

    Updated maintnance message

--- backend/application/views/errors/maintnance.php
+++ backend/application/views/errors/maintnance.php
@@ -1,13 +1,19 @@
-<h2> Database is currently being rebuilt due to the server running out of RAM.</h2>
+<h2> Website cannot function due to Universallis being down</h2>
 <br>
-<h2> This involves getting the sales data from Universallis, putting it on the Redis database and calculating scores for each item based on that data</h2>
-<h2> A port to ScyllaDB is also being developed so this error will stop happening. </h2>
+<h2> Back in November the Universallis service suffered a catastrophic crash</h2>
 <br>
-<h2> The duration of this process varies based on the number of sales recorded in the last 24h. </h2>
+<h2> This website could work while the API is in this state but it'd provide a unacceptable level of service</h2>
 <br>
+<h2> Due to the API stress caused by running this project it has been put on halt until the issues with the Universallis API subsides</h2>
+<br>
+<h2> Once Universallis has been fixed, development and maintnance of the tools will resume</h2>
+<br>
+<h2> Thank you for your understanding</h2>
+<br>
+<h2> To be notified when this project is back up and running, please contact me on discord: Jojo#1337</h2>
 <br>
 <div class="row flex" style="justify-content: space-around">
     <div style="height:600px;width:600px">
-        <img src="https://thenib.com/wp-content/uploads/2019/08/this-is-not-fine-001-dae9d5-1.png" width="600px" height="600px">
+        <img src="https://thenib.com/wp-content/uploads/2019/08/this-is-not-fine-001-dae9d5-1.png" width="400px" height="400px">
     </div>
 </div>
