b7a4f0d6eb17dfc5793d9977d473943c64ea16b0
MTVirux <supersilver-dark@hotmail.com>
2023-05-09 16:38:11 +0000

    Removed python_update from controller as it is in the API

--- backend/application/controllers/Test.php
+++ backend/application/controllers/Test.php
@@ -17,60 +17,6 @@ class Test extends MY_Controller {
 		echo json_encode('ok');
 	}
 
-	public function python_update(){
-		
-		if(empty($_POST['item_id']) || is_null($_POST['item_id'])){
-			echo "No item_id provided";
-			return;
-		}
-
-		if(empty($_POST['world_name']) || is_null($_POST['world_name'])){
-			echo "No world name provided";
-			return;
-		}
-
-		logger("PYTHON_UPDATE", json_encode(array("item_id" => $_POST['item_id'], "world_name" => $_POST['world_name'])));
-
-		$item_id = $_POST['item_id'];
-		$world = $_POST['world_name'];
-
-		$this->load->config('worlds');
-		$worlds_to_use = $this->worlds_to_use();
-
-		//Check if we're tracking the world
-		if(in_array($world, $worlds_to_use)){
-			$this->Redis_ts->calc_item_score($item_id, $world);
-			echo $world.'_'.$item_id. ' updated successfully';
-			return true;
-		}
-		return false;
-		
-	}
-
-	public function update_item_scores(){
-		set_time_limit(600);
-		$this->Redis_ts->global_item_score_update();
-	}
-
-	public function test_craft() {
-		$this->load->model('Item_model', 'Item');
-		$craftable_items = $this->Item->get_craftable_items();
-		pretty_dump($craftable_items);
-	}
-
-	public function worlds_to_use(){
-		return (get_worlds_to_use(	
-										$this->config->item('worlds_to_use'), 
-										$this->config->item('dcs_to_use'), 
-										$this->config->item('regions_to_use'), 
-										$this->config->item('ffxiv_worlds')
-									)
-								);
-	}
-
-
-
-
 
 	public function currency_efficiency_calculator(){
 		$this->load->model("Scylla/Scylla_Item_model", "Scylla_Item");
