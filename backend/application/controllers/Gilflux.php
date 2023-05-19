8184510655d7b968b8d9f622c23ad0945114a2d3
MTVirux <supersilver-dark@hotmail.com>
2023-05-19 11:51:03 +0000

    Updated gilflux controller to just take users to view

--- backend/application/controllers/Gilflux.php
+++ backend/application/controllers/Gilflux.php
@@ -16,65 +16,9 @@ class GilFlux extends MY_Controller{
 	}
 
 
-    public function index($target_location, $craft = null, $limit = null, $page = null){
+    public function index(){
 
-		$target_type = "world";
-
-		$worlds = $this->Scylla_Worlds->get();
-
-		$locations = [];
-
-		foreach($worlds as $world){
-			$locations[$world["region"]][$world["datacenter"]][] = $world["name"];
-		}
-
-
-		foreach($locations as $region => $datacenter_data){
-			if(strtolower($target_location) == strtolower($region)){
-				$target_type = "region";
-			}else{
-				foreach($locations[$region] as $datacenter => $worlds){
-					if(strtolower($target_location) == strtolower($datacenter)){
-						$target_type = "datacenter";
-					}
-				}
-			}
-		}
-
-		if($target_type == "world"){
-
-			//Get ID
-			$world = $this->Scylla_Worlds->get_by_name($target_location);
-			$target_location = $world[0]["id"];
-			$gilflux_ranking = $this->Scylla_gilflux_ranking->get_by_world($target_location);
-
-		}else if($target_type == "datacenter"){
-
-			$gilflux_ranking = $this->Scylla_gilflux_ranking->get_by_datacenter($target_location);
-
-		}else if($target_type == "region"){
-
-			$gilflux_ranking = $this->Scylla_gilflux_ranking->get_by_region($target_location);
-
-		}
-
-		if($craft){
-			//Get all crafted item ids
-			$craftable_item_ids = $this->Scylla_Items->get_craftable_items(TRUE);
-
-			foreach($gilflux_ranking as $gilflux_ranking_item_id => $gilflux_ranking_item){
-				if(!in_array($gilflux_ranking_item["item_id"], $craftable_item_ids)){
-					unset($gilflux_ranking[$gilflux_ranking_item_id]);
-				}
-			}
-		}
-
-		array_multisort(array_column($gilflux_ranking, "ranking_1h"), SORT_DESC, $gilflux_ranking);
-		// TODO: Proper return
-		pretty_dump($gilflux_ranking);
-		
-
-
+		$this->load_view_template('gilflux');
 
 	}
 
