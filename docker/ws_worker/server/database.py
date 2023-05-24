98dca452108f105c2d2fa494d0c604f41c68760d
MTVirux <supersilver-dark@hotmail.com>
2023-05-24 15:02:13 +0000

    Added more IPs to the Scylla Cluster

--- docker/ws_worker/server/database.py
+++ docker/ws_worker/server/database.py
@@ -1,4 +1,4 @@
 from cassandra.cluster import Cluster
 
-SCYLLA_DB_CLUSTER = Cluster(['10.0.0.3'])
+SCYLLA_DB_CLUSTER = Cluster(['10.0.0.3', '10.0.0.4', '10.0.0.5', '10.0.0.6'])
 SCYLLA_DB = SCYLLA_DB_CLUSTER.connect('ffmt')
