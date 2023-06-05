d445b9362d5006989e94dcd6beb2d38141940152
MTVirux <supersilver-dark@hotmail.com>
2023-06-05 09:48:19 +0000

    Added creation of log dirs when executing the sales_importer run script

--- docker/ws_worker/sales_importer/run_importer.sh
+++ docker/ws_worker/sales_importer/run_importer.sh
@@ -7,6 +7,11 @@ cd /sales_importer/python
 # set permissions on log folders
 chmod -R 777 /sales_importer/logs
 
+mkdir -p /sales_importer/logs/action
+mkdir -p /sales_importer/logs/error
+mkdir -p /sales_importer/logs/request
+mkdir -p /sales_importer/logs/debug
+
 # if --detached was passed run detached else run in foreground
 if [ "$1" = "--detached" ]; then
     echo "Running in detached mode"
