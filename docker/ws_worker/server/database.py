30f90f218eca6303795260d5ff3cdd6c0d65a58d
MTVirux <supersilver-dark@hotmail.com>
2023-06-07 15:45:55 +0000

    Removed the 10.0.0.5 scylla node

--- docker/ws_worker/server/database.py
+++ docker/ws_worker/server/database.py
@@ -1,4 +1,4 @@
 from cassandra.cluster import Cluster
 
-SCYLLA_DB_CLUSTER = Cluster(['10.0.0.3', '10.0.0.4', '10.0.0.5'])
+SCYLLA_DB_CLUSTER = Cluster(['10.0.0.3', '10.0.0.4'])
 SCYLLA_DB = SCYLLA_DB_CLUSTER.connect('ffmt')
