7933b565a598ec28876a4242af7c8ea1a6a4ed4f
MTVirux <supersilver-dark@hotmail.com>
2023-10-16 08:45:06 +0000

    Make ws worker connect to scylla node by container name

--- docker/ws_worker/server/database.py
+++ docker/ws_worker/server/database.py
@@ -1,4 +1,3 @@
 from cassandra.cluster import Cluster
 
-SCYLLA_DB_CLUSTER = Cluster(['10.0.0.3', '10.0.0.4'])
-SCYLLA_DB = SCYLLA_DB_CLUSTER.connect('ffmt')
+SCYLLA_DB_CLUSTER = Cluster(['ffmt_scylla_node'])
