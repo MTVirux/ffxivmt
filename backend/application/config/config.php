9ab999adb32467739746f408fed69466ac23389e
MTVirux <supersilver-dark@hotmail.com>
2023-11-15 17:17:11 +0000

    Forced HTTPS on backend requests

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (!empty($_SERVER['HTTP_HOST']) ? 'http://'.$_SERVER['HTTP_HOST'] : 'https://'.'mtvirux.app');
+$config['base_url'] = (!empty($_SERVER['HTTP_HOST']) ? 'https://'.$_SERVER['HTTP_HOST'] : 'https://'.'mtvirux.app');
 
 /*
 |--------------------------------------------------------------------------
