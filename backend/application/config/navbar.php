7a3b932999ac9b32c16679ab1407533aec501772
MTVirux <joaofraga96@outlook.com>
2024-12-19 19:17:11 +0000

    Redesign navbar

--- backend/application/config/navbar.php
+++ backend/application/config/navbar.php
@@ -5,20 +5,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['navbar_structure'] = 
 [
 
-    "Gilflux" => array(
-                        "name" => "Gilflux",
-                        "link" => 'gilflux/index',
-                        "description" => "Show's the top items that moved the most gil for the selected World, DC or Region."
-                    ),
-    "Item Product Profit Solver" => array(
-                                            "name" => "Item Product Profit Solver",
-                                            "link" => 'tools/item_product_profit_calculator',
-                                            "description" => "The most profitable craft for a certain material."
-                                        ),
-    "Currency Efficiency Calculator" =>  array(
-                                                "name" => "Currency Efficiency Calculator",
-                                                "link" => 'tools/currency_efficiency_calculator',
-                                                "description" => "The most profitable way to spend your currency."
-                                            ),
+    "Gilflux" => 
+    array(
+        "name" => "Gilflux",
+        "link" => 'gilflux/index',
+        "description" => "Show's the top items that moved the most gil for the selected World, DC or Region."
+    ),
+
+
+    "Currency Efficiency Calculator" =>  
+    array(
+            "name" => "Currency Efficiency Calculator",
+            "link" => 'tools/currency_efficiency_calculator',
+            "description" => "The most profitable way to spend your currency."
+        ),
+
+
+    "Item Product Profit Solver" => 
+    array(
+        "name" => "Item Product Profit Solver",
+        "link" => 'tools/item_product_profit_calculator',
+        "description" => "The most profitable craft for a certain material."
+    ),
+
 
 ];
