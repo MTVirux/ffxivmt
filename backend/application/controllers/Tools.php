3822cd43d27617daefa96dbe4b2a9da67d5bf2ae
MTVirux <joaofraga96@outlook.com>
2024-12-19 19:23:19 +0000

    Added caching and redirecting to Tools controller

--- backend/application/controllers/Tools.php
+++ backend/application/controllers/Tools.php
@@ -6,15 +6,27 @@ class Tools extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
+		$this->load->library('session');
 	}
 
 	public function item_product_profit_calculator(){
 		$data['message'] = "Please enter a search term...";
+		$data['session'] = $this->session->userdata();
 		$this->load_view_template('tools/item_product_profit_calculator', $data);
 		return;
 	}
 
 	public function currency_efficiency_calculator(){
-		$this->load_view_template('tools/currency_efficiency_calculator');
+		$data["session"] = $this->session->userdata();
+		$this->load_view_template('tools/currency_efficiency_calculator', $data);
+	}
+
+	/*
+	Redirects
+	*/
+
+	// Redirects currency_profit_calculator to currency_efficiency_calculator
+	public function currency_profit_calculator(){
+		currency_efficiency_calculator();
 	}
 }
