ed4be3d4d12ea8ee145d48257edf469df95cf702
MTVirux <joaofraga96@outlook.com>
2025-01-11 15:11:09 +0000

    Added option to save script changes

--- swap_scripts.sh
+++ swap_scripts.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
-echo "Select what scripts to bring to current directory:"
+echo "Select what scripts to bring to the current directory:"
 echo "[1] - Linux"
 echo "[2] - Clean up existing scripts"
-echo 
-read choice
+echo "[3] - Save scripts"
+echo
+read -r choice
 
-if [ $choice -eq 1 ] || [ $choice -eq 2 ]; then
-  # Move swap_scripts.sh to a safe location
+# Create a temporary directory and move swap_scripts.sh to a safe location
+preserve_swapping_script() {
   mkdir -p ./temp
-  mv swap_scripts.sh ./temp/
+  mv -f swap_scripts.sh ./temp/
+}
 
-  # Remove existing .sh, .bat, and .ps files from the current directory
-  rm -f *.sh
-  rm -f *.bat
-  rm -f *.ps
-
-  if [ $choice -eq 1 ]; then
-    cp ./scripts/sh/* .
-    chmod +x ./*.sh
-  elif [ $choice -eq 2 ]; then
-    echo "All .sh / .bat / .ps1 scripts removed from current directory..."
-  fi
-
-  # Move swap_scripts.sh back to the current directory
-  mv temp/swap_scripts.sh .
+restore_swap_script() {
+  mv -f ./temp/swap_scripts.sh .
   rm -rf ./temp
-else
-  echo "Invalid selection"
-fi
+}
+
+case $choice in
+  1)
+    preserve_swapping_script
+    # Remove existing scripts
+    rm -f *.sh *.bat *.ps
+    # Copy Linux scripts and set executable permissions
+    cp -f ./scripts/sh/* .
+    chmod +x ./*.sh
+    restore_swap_script
+    ;;
+  2)
+    preserve_swapping_script
+    # Remove existing scripts
+    rm -f *.sh *.bat *.ps
+    echo "All .sh / .bat / .ps1 scripts removed from the current directory..."
+    restore_swap_script
+    ;;
+  3)
+    preserve_swapping_script
+    # Move scripts to their respective folders
+    mv -f *.sh ./scripts/sh/
+    mv -f *.ps1 ./scripts/ps1/
+    mv -f *.bat ./scripts/bat/
+    restore_swap_script
+    ;;
+  *)
+    echo "Invalid selection"
+    ;;
+esac
