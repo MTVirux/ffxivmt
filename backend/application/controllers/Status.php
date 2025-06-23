da90225b63c55addd12487187425700be0729d26
MTVirux <joaofraga96@outlook.com>
2025-06-23 06:43:36 +0000

    Updated status checks for scylla

--- backend/application/controllers/Status.php
+++ backend/application/controllers/Status.php
@@ -6,25 +6,44 @@ class Status extends MY_Controller {
     public function __construct(){
 
         parent::__construct();
-        $this->load->model('Status_model');
+        $this->load->model('Scylla/Scylla_Status_model');
 
     }
 
     public function index(){
 
-        $sql_databases = ['ffxiv_db'];
-
-        $data = [];
-
-        foreach($sql_databases as $sql_database){
-            $data['SQL'][$sql_database] = $this->Status_model->get_sql($sql_databases);
+        #Check cache and return if it exists
+        $cache_key = 'scylla_status';
+        $cached_data = $this->cache->get($cache_key);
+        if ($cached_data) {
+            $data = array(
+                'status' => $cached_data,
+                'code' => 200
+            );
+            echo json_encode($data);
+            http_response_code($data['code']);
+            return;
         }
 
-        $data['Redis'] = $this->Status_model->get_redis();
+        #check if scylla is up if there was no cache
+        if ($this->Scylla_Status_model->get_scylla($this->config->item('sql_databases'))) {
+            $data = array(
+                'status' => 'Scylla is up',
+                'code' => 200
+            );
+        } else {
+            $data = array(
+                'status' => 'Scylla is down',
+                'code' => 500
+            );
+        }
 
-        pretty_dump($data);die();
-        
-        //$this->load_view_template'status', $data);
-        return true;
+        echo "" . json_encode($data) . "";
+        http_response_code($data['code']);
+
+        #Set cache for 10 seconds
+        $this->cache->save($cache_key, $data['status'], 10);
+        return;
+
     }
 }
